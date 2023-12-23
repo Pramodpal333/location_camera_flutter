163e75af37dc04335e17ae2058f69568d3d65994
laxmanPal <laxi27902@gmail.com>
2023-12-23 12:26:03 +0530

    camera added

--- lib/features/camera/pages/camera.dart
+++ lib/features/camera/pages/camera.dart
@@ -1,17 +1,57 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
 class CameraPage extends StatefulWidget {
-  const CameraPage({Key? key}) : super(key: key);
+  final List<CameraDescription> cameras;
+   CameraPage( {Key? key, required this.cameras,}) : super(key: key);
 
   @override
   State<CameraPage> createState() => _CameraPageState();
 }
 
 class _CameraPageState extends State<CameraPage> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
 
+  @override
+  void initState() {
+    super.initState();
+    _controller = CameraController(widget.cameras[0], ResolutionPreset.ultraHigh);
+    _controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+          // Handle access errors here.
+            break;
+          default:
+          // Handle other errors here.
+            break;
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    var w= MediaQuery.of(context).size.width;
+    var h= MediaQuery.of(context).size.height;
+    if (!_controller.value.isInitialized) {
+      return const Center(child: CircularProgressIndicator());
+    }
+    return MaterialApp(
+      home: CameraPreview(_controller),
+    );
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
   }
 }
